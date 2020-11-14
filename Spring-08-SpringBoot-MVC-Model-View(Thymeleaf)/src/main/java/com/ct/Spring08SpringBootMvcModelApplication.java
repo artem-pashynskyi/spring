379ababe87a8e118011af8bc9fda42114e34594f
artem-pashynskyi <mailofartem@gmail.com>
2020-11-14 14:52:33 -0500

    SpringBoot MVC - Model - View

---
+++ Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/ct/Spring08SpringBootMvcModelApplication.java
@@ -0,0 +1,13 @@
+package com.ct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08SpringBootMvcModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring08SpringBootMvcModelApplication.class, args);
+	}
+
+}
---
+++ Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/ct/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.ct.enums;
+
+public enum Gender {
+    MALE, FEMALE;
+}
