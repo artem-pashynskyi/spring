ad64b0ba1c5008b4598096f0e679ad81324bb028
artem-pashynskyi <mailofartem@gmail.com>
2020-12-15 21:16:34 -0500

    ORM Mapping Many to One

---
+++ Spring-09-ORM-Mapping-ManyToOne/src/test/java/com/orm3/Spring09OrmMappingManyToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orm3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09OrmMappingManyToOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
