4102c52228fed1ee78406dfd09ed4b3a3b2ab014
artem-pashynskyi <mailofartem@gmail.com>
2020-12-15 21:16:13 -0500

    ORM Mapping One to One

---
+++ Spring-09-ORM-Mapping-OneToOne/src/test/java/com/orm2/Spring09OrmMappingOneToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orm2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09OrmMappingOneToOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
