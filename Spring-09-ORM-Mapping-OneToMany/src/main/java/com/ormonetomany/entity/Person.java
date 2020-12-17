2e7e8bce223f1e1d8a486b7287c895e46ab6e98e
artem-pashynskyi <mailofartem@gmail.com>
2020-12-16 22:15:53 -0500

    ORM Mapping One To Many

---
+++ Spring-09-ORM-Mapping-OneToMany/src/main/java/com/ormonetomany/entity/Person.java
@@ -0,0 +1,34 @@
+package com.ormonetomany.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @OneToMany(mappedBy = "person")
+    private List<Address> address;
+
+    //    Case 1
+//    @OneToMany(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "person_id")
+//    private List<Address> address;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
