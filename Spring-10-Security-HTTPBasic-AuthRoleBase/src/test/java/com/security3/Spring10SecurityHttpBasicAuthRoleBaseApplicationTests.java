99055f2cbead84574075d19b396fb104df5728f4
artem-pashynskyi <mailofartem@gmail.com>
2021-01-17 10:43:20 -0500

    Security - HttpBasic AuthRoleBase

---
+++ Spring-10-Security-HTTPBasic-AuthRoleBase/src/test/java/com/security3/Spring10SecurityHttpBasicAuthRoleBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring10SecurityHttpBasicAuthRoleBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
