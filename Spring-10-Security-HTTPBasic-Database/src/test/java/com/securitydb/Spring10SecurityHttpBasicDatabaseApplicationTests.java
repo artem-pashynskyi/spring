6cb8a1666a9cddcd9438b857ac8c74389edc4b57
artem-pashynskyi <mailofartem@gmail.com>
2021-01-17 14:03:16 -0500

    Security - HttpBasic Database

---
+++ Spring-10-Security-HTTPBasic-Database/src/test/java/com/securitydb/Spring10SecurityHttpBasicDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securitydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring10SecurityHttpBasicDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
