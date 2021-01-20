6975b9ca3197c16f332b5a1ff9678dd1dc233817
artem-pashynskyi <mailofartem@gmail.com>
2021-01-19 19:14:39 -0500

    Security Form Auth

---
+++ Spring-10-Security-FormAuth/src/main/java/com/securityform/securityConfig/UserPrincipleDetailsService.java
@@ -0,0 +1,24 @@
+package com.securityform.securityConfig;
+
+import com.securityform.entity.User;
+import com.securityform.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPrincipleDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserPrincipleDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(s);
+        return new UserPrincipal(user);
+    }
+}
