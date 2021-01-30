36c11ef3cfd3170d3b3dc80d422496f4444fe311
artem-pashynskyi <mailofartem@gmail.com>
2021-01-30 14:17:43 -0500

    API REST WebFlux WebClient

---
+++ Spring-11-Rest-Jackson/src/main/java/com/jackson/controller/WebFluxController.java
@@ -0,0 +1,112 @@
+package com.jackson.controller;
+
+import com.jackson.entity.Genre;
+import com.jackson.entity.MovieCinema;
+import com.jackson.repository.GenreRepository;
+import com.jackson.repository.MovieCinemaRepository;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class WebFluxController {
+
+    private MovieCinemaRepository movieCinemaRepository;
+    private GenreRepository genreRepository;
+
+    public WebFluxController(MovieCinemaRepository movieCinemaRepository, GenreRepository genreRepository) {
+        this.movieCinemaRepository = movieCinemaRepository;
+        this.genreRepository = genreRepository;
+    }
+
+    private WebClient webClient = WebClient
+                                    .builder()
+                                    .baseUrl("http://localhost:8080")
+                                    .build();
+
+    @GetMapping("/flux-movie-cinemas")
+    public Flux<MovieCinema> readAllCinemaFlux() {
+        return Flux.fromIterable(movieCinemaRepository.findAll());
+    }
+
+    @GetMapping("/mono-movie-cinema/{id}")
+    public Mono<MovieCinema> readById(@PathVariable("id") Long id) {
+        return Mono.just(movieCinemaRepository.findById(id).get());
+    }
+
+    @GetMapping("/mono-movie-cinema")
+    public Mono<MovieCinema> readByIdRequestParam(@RequestParam("id") Long id) {
+        return Mono.just(movieCinemaRepository.findById(id).get());
+    }
+
+    @PostMapping("/create-genre")
+    public Mono<Genre> createGenre(@RequestBody Genre genre) {
+        return Mono.just(genreRepository.save(genre));
+    }
+
+    @PutMapping("/update-genre")
+    public Mono<Genre> updateGenre(@RequestBody Genre genre) {
+        Genre foundGenre = genreRepository.save(genre);
+        return Mono.just(foundGenre);
+    }
+
+    @DeleteMapping("/delete-genre/{id}")
+    public Mono<Void> deleteGenre(@PathVariable("id") Long id) {
+        genreRepository.deleteById(id);
+        return Mono.empty();
+    }
+
+
+    //-----------------------WEBCLIENT EXAMPLE--------------------------------
+    @GetMapping("/flux")
+    public Flux<MovieCinema> readWithWebClient() {
+        return webClient
+                .get()
+                .uri("/flux-movie-cinemas")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .retrieve()
+                .bodyToFlux(MovieCinema.class);
+    }
+
+    @GetMapping("/mono/{id}")
+    public Mono<MovieCinema> readMonoWithWebClient(@PathVariable("id") Long id) {
+        return webClient
+                .get()
+                .uri("/mono-movie-cinema/{id}", id)
+                .retrieve()
+                .bodyToMono(MovieCinema.class);
+    }
+
+    @GetMapping("/mono-qp")
+    public Mono<MovieCinema> readMonoWithWebClientRequestParam(@RequestParam("id") Long id) {
+        return webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/mono-movie-cinema").queryParam("id", id)
+                    .build())
+                .retrieve()
+                .bodyToMono(MovieCinema.class);
+    }
+
+    @PostMapping("/create")
+    public Mono<Genre> createWebClient(@RequestBody Genre genre) {
+        return webClient
+                .post()
+                .uri("/create-genre")
+                .body(Mono.just(genre), Genre.class)
+                .retrieve()
+                .bodyToMono(Genre.class);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Mono<Void> deleteWebClient(@PathVariable("id") Long id) {
+        return webClient
+                .delete()
+                .uri("/delete-genre/{id}", id)
+                .retrieve()
+                .bodyToMono(Void.class);
+    }
+
+}
